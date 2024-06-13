e95a2fe764c4674934b3e1689ff229a6016bcbb5
Lectral <120367292+Lec7ral@users.noreply.github.com>
2024-06-12 23:49:51 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,5 @@ xbot = Client(
 	bot_token=Config.BOT_TOKEN,
 	plugins=dict(root="plugins"),
 )
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 xbot.run()
